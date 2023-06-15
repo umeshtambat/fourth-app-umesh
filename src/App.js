ea0164f5099188d3b362163c20e3ce496234f392
Umesh <Umeshtambat55@gmail.com>
2023-06-15 09:42:07 +0530

    properties

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-function App () {
+function App() {
   return (
     <>
-    <h1>Props Demo </h1>
+      <h1>Props Demo </h1>
 
-    {/* <MessageDemo />
+      {/* <MessageDemo />
     <MessageDemo message="Hello"/> */}
-    <MessageDemo username="umesh" />
-    <MessageDemo username="vaibhav" />
-    <MessageDemo username="tushar" />
-    
-    
-    
+      <MessageDemo name="umesh" email="u@gmail.com" />
+      <MessageDemo name="vaibhav" email="v@gmail.com" />
+      <MessageDemo name="tushar" email="t@gmail.com" />
     </>
-  )
+  );
 }
 
-
 //how to make this dynamic
-function MessageDemo (props) {
+function MessageDemo({ name, email }) {
   //console.log("I am insite messagedemo", props);
 
-  return(
+  return (
     <>
-    <h1>Hello {props.username}</h1>
+      <h1>
+        Hello  {email} {name}
+      </h1>
     </>
-  )
-
+  );
 }
 
 export default App;

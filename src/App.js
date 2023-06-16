6c1efde8baf1a9a260fc66d87dd1aea03e647865
Umesh <Umeshtambat55@gmail.com>
2023-06-16 11:50:11 +0530

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { useRef, useState } from "react";
-import MyTodo from "./pages/MyTodo.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MyTodo from "./pages/MyTodo";
 import MyRegistration from "./pages/MyRegistration";
 
 function App() {
   return (
     <>
-      <MyTodo />
-      <hr />
-      <MyRegistration />
+      <BrowserRouter>
+        <Routes>
+          {/** 1 Route means 1 Page */}
+          <Route path="/" element={<h1>Hello</h1>} />
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/todo" element={<MyTodo />} />
+          <Route path="/registration" element={<MyRegistration />} />
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
